e04f309ac92bbf971ae347189968b85995838b83
Enoch Klufio <enochmk94@gmail.com>
2022-05-11 17:25:36 +0000

    refactor: re-arrange the order of sidebar

--- src/components/Sidebars/Sidebar.jsx
+++ src/components/Sidebars/Sidebar.jsx
@@ -22,19 +22,19 @@ function Sidebar() {
 						</Link>
 					</li>
 					<li className='nav-item'>
-						<Link className='nav-link text-white' to='/randomizer'>
-							<div className='text-white text-center me-2 d-flex align-items-center justify-content-center'>
-								<i className='material-icons opacity-10'>table_view</i>
-							</div>
-							<span className='nav-link-text ms-1'>Randomizer</span>
-						</Link>
-					</li>
-					<li className='nav-item'>
 						<Link className='nav-link text-white' to='/loader'>
 							<div className='text-white text-center me-2 d-flex align-items-center justify-content-center'>
 								<i className='material-icons opacity-10'>receipt_long</i>
 							</div>
 							<span className='nav-link-text ms-1'>Loader</span>
+						</Link>
+					</li>
+					<li className='nav-item'>
+						<Link className='nav-link text-white' to='/randomizer'>
+							<div className='text-white text-center me-2 d-flex align-items-center justify-content-center'>
+								<i className='material-icons opacity-10'>table_view</i>
+							</div>
+							<span className='nav-link-text ms-1'>Randomizer</span>
 						</Link>
 					</li>
 				</ul>
