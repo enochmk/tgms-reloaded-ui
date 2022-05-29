eab263eb00baebb4b2446d6e0dd1619dc95748bd
Enoch Klufio <enochmk94@gmail.com>
2022-05-29 20:39:10 +0000

    refactor: update endpoint

--- src/components/Cards/Fileupload.jsx
+++ src/components/Cards/Fileupload.jsx
@@ -42,7 +42,7 @@ function Fileupload({ setStatistics, setIsLoading }) {
     try {
       const config = {
         method: 'post',
-        url: 'http://localhost:8000/api/tgms2/loader',
+        url: 'http://10.81.9.68:8000/api/tgms2/loader',
         headers: { 'Content-Type': 'multipart/form-data' },
         data: data,
       };
--- src/services/fetchRandomWinners.js
+++ src/services/fetchRandomWinners.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const fetchRandomWinners = async (numberOfWinnersInput) => {
   const response = await axios.get(
-    `http://localhost:8000/api/tgms2/randomizer?count=${numberOfWinnersInput}`,
+    `http://10.81.9.68:8000/api/tgms2/randomizer?count=${numberOfWinnersInput}`,
   );
 
   return response.data;
