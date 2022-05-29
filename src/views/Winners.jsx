bda081d41785ec677697104e5ff386fd471c202f
Enoch Klufio <enochmk94@gmail.com>
2022-05-29 20:11:52 +0000

    feat: winners page complete

--- src/views/Winners.jsx
+++ src/views/Winners.jsx
@@ -1,16 +1,57 @@
-import React from 'react';
-import { Container } from 'reactstrap';
+import { useContext, useEffect } from 'react';
+import { Card, CardBody, CardTitle, Container } from 'reactstrap';
+import { DrawContext } from '../context/DrawContext';
+import fetchStatistics from '../services/fetchStatistics';
+import { toast } from 'react-toastify';
+import second from 'axios';
 
 function Winners() {
+  const drawContext = useContext(DrawContext);
+  const winners = drawContext.data.winners;
+
+  useEffect(() => {
+    fetchStatistics()
+      .then((data) => {
+        drawContext.setStatistics(data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          toast.error(error.response.data.message);
+        } else {
+          toast.error('Something went wrong');
+        }
+      });
+  }, []);
+
   return (
-    <div className="py-8 pt-5">
-      <Container fluid className="justify-content-center">
-        <div className="mt-4 p-5 text-white rounded text-center">
-          <h1 className="text-bold">Winners</h1>
-          <p className="lead text-dark">This is page for all Winners</p>
+    <Container className="mt-5 pt-5">
+      <div className="row justify-content-center pt-5">
+        <div className="col-12">
+          <Card
+            className="scroll"
+            // style={{ height: '100%', maxHeight: '100vh' }}
+          >
+            <CardBody>
+              <CardTitle tag="h1" className="text-muted text-center">
+                Winners
+              </CardTitle>
+              <div className="justify-content-start ">
+                <ul
+                  className="list-unstyled card-columns"
+                  style={{ columnCount: 8 }}
+                >
+                  {winners.map((winner, index) => (
+                    <li key={index} className="text-dark">
+                      <h4>0{winner}</h4>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </CardBody>
+          </Card>
         </div>
-      </Container>
-    </div>
+      </div>
+    </Container>
   );
 }
 
